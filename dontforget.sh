64e4f571a16aa4b4f5059b3484f70f298faf1eb5
lrr68 <lucca@luccaaugusto.xyz>
2021-08-18 08:15:50 -0300

    make executable and add fetching email appointments

--- dontforget.sh
+++ dontforget.sh
@@ -5,6 +5,84 @@
 #-Can notify via email your close appointments
 
 appointmentsfile="$HOME/Documents/appointments.csv"
+header="date,time,description"
+#email is used to ssh to server and fetch remote commands
+email="lucca@luccaaugusto.xyz"
+#subject is used to filter emails that contain commands
+subject="Dontforget"
+
+fetchemailappointments()
+{
+	state=0
+	date=""
+	time=""
+	description=""
+	body=""
+	mailquery="${0##*/}.mailquery"
+
+	(ssh $email "doveadm fetch 'body date.received' mailbox inbox subject $subject > mailquery &&
+		doveadm flags add '\Seen' mailbox inbox unseen subject $subject &&
+		doveadm move Trash mailbox inbox seen subject $subject &&
+		cat mailquery" > "$mailquery")
+	# query the server for unseen emails with subject=$subject
+	# outputs email body and date.received to a file so line breaks are preserved
+	# marks these emails as seen
+	# cats the file so we get it's contents locally
+	while IFS= read -r line || [ -n "$line" ]
+	do
+		case "$state" in
+			0) #expect body
+				[ "${line%%:*}" = "body" ] && state=1
+				;;
+			1) #read until find spend or receive
+				#concatenate line for future error reporting
+				body="$body|$line"
+				if [ "${line%% *}" = "add" ]
+				then
+					state=2
+					date="${line%% *}"
+					time="${line#* }"
+					time="${amount%% *}"
+					description="${line#* }"
+					description="${t_type#* }"
+				elif [ "${line%%:*}" = "date.received" ]
+				then
+					#read until date and did not get command, something is wrong with the email
+					{
+						echo "${0##*/} ERROR:"
+						echo "    Command not found in email"
+						echo "    body: $body"
+						echo "====Please do this one manually"
+					} >> "$HOME/.${0##*/}.log"
+
+					state=0
+					body=""
+				fi
+				;;
+			2) #read until find date.received
+				if [ "${line%%:*}" = "date.received" ]
+				then
+					addappointment $date $time $description
+					#Reset to read next
+					state=0
+					body=""
+				fi
+				;;
+		esac
+
+	done < "$mailquery"
+	rm "$mailquery"
+
+	[ -e "$HOME/.${0##*/}.log" ] &&
+		sed 's/|/\n    /g' < "$HOME/.${0##*/}.log" > "$HOME/.${0##*/}.log.aux" &&
+		mv "$HOME/.${0##*/}.log.aux" "$HOME/.${0##*/}.log" &&
+		notify-send "${0##*/} ERROR" "There were errors processing email logged appointments. See $HOME/.${0##*/}.log"
+}
+
+fetchupdates()
+{
+	fetchemailappointments
+}
 
 addappointment()
 {
